8df141b2cfd446763e796c5c5ea848df3e70f003
Professor Vinícius <vinicius.pelizzari.silva@escola.pr.gov.br>
2025-10-03 07:58:31 -0300

    Add files via upload

--- sequencia-ecf.py
+++ sequencia-ecf.py
@@ -1,8 +1,16 @@
+import os
+import sys 
+
 print("*** Mini Questionário ***\n")
 nome = input("Qual seu nome?\n")
 idade = input("Qual a sua idade?\n")
 genero = input("Você é homem ou mulher?\n")
 endereco = input("Qual o seu endereço?\n")
 
+os.system('cls')
+
+print("*** Mini Questionário ***\n")
 print("Olá, ", nome, "\n")
-print("\n")
+print("Idade: ", idade, "\n")
+print("Gênero: ", genero, "\n")
+print("Endereço: ", endereco, "\n")
